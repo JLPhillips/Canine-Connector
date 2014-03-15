23820d232c230c77998b6d2d216748c530130e2f
JL Phillips <phillipsj@pop.belmont.edu>
2014-03-15 07:37:38 -0500

    Added ENV variables

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -33,9 +33,9 @@ class DogsController < InheritedResources::Base
       text_end = "."
     end
 
-    twilio_sid = "?" # "ENV[TWILIO_SID]"
-    twilio_token = "?"
-    twilio_phone_number = "?"
+    twilio_sid = ENV["TWILIO_SID"]
+    twilio_token = ENV["TWILIO_TOKEN"]
+    twilio_phone_number = ENV["TWILIO_PHONE_NUMBER"]
 
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ CanineConnector::Application.configure do
   ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => "?",
-  :password             => '?',
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"],
   :authentication       => "plain",
   :enable_starttls_auto => true
   }
